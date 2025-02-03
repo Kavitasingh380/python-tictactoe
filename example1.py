83e7febe13931ccbe036b09bf7f496da4b9a61be
kavita <kavitasingh380@gmail.com>
2025-02-04 00:58:58 +0530

    initial commit

---
+++ example1.py
@@ -0,0 +1,12 @@
+print("hello python")
+
+def display(row1,row2,row3):
+    print(row1)
+    print(row2)
+    print(row3)
+
+display([1,2,3],[1,2,3],[1,2,3])
+
+# result = input("please enter a value: ")
+
+# type(result)
